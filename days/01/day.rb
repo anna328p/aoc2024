aba39a1d6788e651db6e43d798ccbb11f251caf2
Anna Kudriavtsev <anna328p@gmail.com>
2024-12-01 19:08:23 -0600

    port day 1 to new stuff

---
+++ days/01/day.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'aoc'
+
+module Day1
+  extend AoC::Solution
+
+  def self.day = 1
+
+  def self.parse(input)
+    input.lines.map { |l| l.split.map(&:to_i) }.transpose.map(&:sort)
+  end
+
+  def self.one(lists)
+    lists[0].zip(lists[1]).map { |a, b| (a - b).abs }.sum
+  end
+
+  def self.two((l, r))
+    counts = r.tally
+
+    l.map { _1 * (counts[_1] || 0) }.sum
+  end
+end
+
+if (input_filename = ARGV[1])
+  Day1.run(input_filename)
+else
+  Day1.run
+end
